a84d82c49401d260073b4a3d47e4a4157b0b7aaf
Pat  Pignataro <pat6284@msn.com>
2019-04-04 12:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,19 +15,6 @@ function insertionSort(array){
 
 }
 
-// function findMinAndRemove(array){
-//   let min = array[0]
-//   let minIndex = 0
-//   for (let i = 0; i < array.length; i++){
-//     if (array[i] < min){
-//       min = array[i]
-//       minIndex = i
-//     }
-//   }
-//   array.splice(minIndex, 1)
-//   return min
-// }
-
 // function insertionSort(array){
 //   let sorted = []
 
